9dc979faa65af2b3b083c06399b68a64f27a3269
athensbird <markwang@utexas.edu>
2026-06-26 11:24:53 -0500

    add button group

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -3,6 +3,11 @@ import { useFetch } from '../hooks/useFetch';
 import { BASE_ENDPOINT } from '../assets/configs';
 import CardDetails from './CardDetails';
 
+enum Direction {
+    PREV,
+    NEXT,
+}
+
 const Container = () => {
     const [activeId, setActiveId] = useState("cc-001");
     const [privacyMode, togglePrivacyMode] = useState(false);
@@ -10,13 +15,29 @@ const Container = () => {
     const [cardList, error, loading] = useFetch(`${BASE_ENDPOINT}/cards`);
     const activeCard = cardList?.find(card => card.id === activeId);
 
-    console.log({cardList});
+    const rotateCard = (dir: Direction) => {
+        if (!cardList.length) return;
+        const curIdx = cardList.findIndex(card => card.id === activeId);
+        const safeIdx = curIdx === -1 ? 0 : curIdx;
+        let nextIdx: number;
+        if (dir === Direction.PREV) {
+            nextIdx = safeIdx !== 0 ? safeIdx - 1 : cardList.length - 1;
+        } else {
+            nextIdx = (safeIdx + 1) % cardList.length;
+        }
+        const newCard = cardList[nextIdx];
+        if(newCard) setActiveId(newCard.id);
+    }
 
     if (error) return <p>There's an error: {error.message | 'unknown error'}</p>
     if (loading) return <p>Loading...</p>
     return (
         <div className='container'>
             {activeCard && <CardDetails {...activeCard}/>}
+            <div className='buttons'>
+                <button onClick={() => rotateCard(Direction.PREV)}>Previous</button>
+                <button onClick={() => rotateCard(Direction.NEXT)}>Next</button>
+            </div>
         </div>
     );
 }
