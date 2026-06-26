7d943a32d1fd3ef57fde8dafd4cbfaacaeca3a0b
athensbird <markwang@utexas.edu>
2026-06-26 14:01:38 -0500

    remove boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import Container from './components/Container'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+const App = () =>
     <>
       <section id="center">
         <h2>Credit Card View</h2>
         <Container />
       </section>
-
-
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
-}
+
 
 export default App
